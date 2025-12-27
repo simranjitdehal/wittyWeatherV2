870d8bb9fefc071281b36286494f89d024f6ac74
Simranjit Singh Dehal <simranjitsinghdehal@gmail.com>
2025-12-27 20:02:40 +0530

    added redis_url and removed separate vars

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,9 +1,13 @@
 import redis
 import os
 
-redis_client = redis.Redis(
-    host= os.getenv('REDISHOST'),
-    port=int(os.getenv('REDISPORT')),
-    db=0,
-    decode_responses=True  # auto-decode bytes to strings
+# redis_client = redis.Redis(
+#     host= os.getenv('REDISHOST'),
+#     port=int(os.getenv('REDISPORT')),
+#     db=0,
+#     decode_responses=True  # auto-decode bytes to strings
+# )
+redis_client = redis.from_url(
+    os.getenv("REDIS_URL"),
+    decode_responses=True
 )
